79e1660e87a7af4f8584d7c80acfa19d94ad55eb
Ryan Lerch <rlerch@redhat.com>
2016-09-22 15:48:24 +1000

    added a bit of javascript to add some bootstrap classes to the edit page

--- Fedora/resources/main.js
+++ Fedora/resources/main.js
@@ -1,1 +1,8 @@
 /* JavaScript for the Example skin */
+$( document ).ready(function() {
+    $("#wpSave").addClass("btn btn-primary");
+    $("#wpPreview").addClass("btn btn-secondary");
+    $("#wpDiff").addClass("btn btn-secondary");
+    $("#wpTextbox1").addClass("form-control");
+    $("#wpSummary").addClass("form-control");
+});
