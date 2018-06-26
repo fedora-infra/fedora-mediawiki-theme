b9c59d2a25697e6d6836ad373006597ff856733d
Rick Elrod <rick@elrod.me>
2018-06-26 18:13:52 -0400

    Make headers have permalinks next to them
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- Fedora/resources/main.js
+++ Fedora/resources/main.js
@@ -6,4 +6,13 @@ $( document ).ready(function() {
     $("#wpTextbox1").addClass("form-control");
     $("#wpSummary").addClass("form-control");
     mw.loader.load( 'https://apps.fedoraproject.org/global/fedora-bootstrap-1.0.1/fedora-bootstrap.js' );
+
+    // https://pagure.io/fedora-infrastructure/issue/6912
+    $('.mw-headline').each(
+      function(f) {
+        $(this).prepend(
+          '<a href="#' + $(this).attr('id') + '">' + String.fromCodePoint(128279) + '</a> '
+        );
+      }
+    );
 });
